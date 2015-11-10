4a37701b09dab97e124283ea320f9a5256d009cf
Nicolai Stawinoga <n.stawinoga13@imperial.ac.uk>
2015-11-10 17:04:42 +0000

    Initial commit

---
+++ RecursionLibrary.java
@@ -0,0 +1,22 @@
+public class RecursionLibrary {
+
+  public static int greatestCommonDivisor(int x, int y) {
+    // TODO: Implement this method
+    return -1;
+  }
+
+  public static boolean isPrime(int n) {
+    // TODO: Implement this method
+    return false;
+  }
+
+  public static int sumSquareDigits(int n) {
+    // TODO: Implement this method
+    return -1;
+  }
+
+  public static boolean isHappy(int n) {
+    // TODO: Implement this method
+    return false;
+  }
+}
